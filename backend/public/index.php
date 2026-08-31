7e3b9ef198d32b2328d0ab1a6cac4f163ee785df
umgaddafi <gaddafiumar4445@gmail.com>
2026-08-31 14:35:14 +0100

    Deploy to cPanel - 2026-08-31 14:35:14

--- backend/public/index.php
+++ backend/public/index.php
@@ -1,20 +1,31 @@
 <?php
 
-use Illuminate\Foundation\Application;
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-    require $maintenance;
+try {
+    // Determine if the application is under maintenance...
+    if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+        require $maintenance;
+    }
+
+    // Register the Auto Loader...
+    require __DIR__.'/../vendor/autoload.php';
+
+    // Bootstrap Laravel and handle the request...
+    (require_once __DIR__.'/../bootstrap/app.php')
+        ->handleRequest(Symfony\Component\HttpFoundation\Request::capture());
+} catch (\Throwable $e) {
+    header('Content-Type: application/json', true, 500);
+    echo json_encode([
+        'error' => $e->getMessage(),
+        'file' => $e->getFile(),
+        'line' => $e->getLine(),
+        'trace' => $e->getTraceAsString()
+    ]);
+    exit;
 }
-
-// Register the Composer autoloader...
-require __DIR__.'/../vendor/autoload.php';
-
-// Bootstrap Laravel and handle the request...
-/** @var Application $app */
-$app = require_once __DIR__.'/../bootstrap/app.php';
-
-$app->handleRequest(Request::capture());
