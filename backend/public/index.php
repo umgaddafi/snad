b2847c8e2c96468ad2210696b0d7a393aa86e9b3
umgaddafi <gaddafiumar4445@gmail.com>
2026-08-31 14:45:08 +0100

    Deploy to cPanel - 2026-08-31 14:45:08

--- backend/public/index.php
+++ backend/public/index.php
@@ -1,31 +1,17 @@
 <?php
 
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-try {
-    // Determine if the application is under maintenance...
-    if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-        require $maintenance;
-    }
-
-    // Register the Auto Loader...
-    require __DIR__.'/../vendor/autoload.php';
-
-    // Bootstrap Laravel and handle the request...
-    (require_once __DIR__.'/../bootstrap/app.php')
-        ->handleRequest(Symfony\Component\HttpFoundation\Request::capture());
-} catch (\Throwable $e) {
-    header('Content-Type: application/json', true, 500);
-    echo json_encode([
-        'error' => $e->getMessage(),
-        'file' => $e->getFile(),
-        'line' => $e->getLine(),
-        'trace' => $e->getTraceAsString()
-    ]);
-    exit;
+// Determine if the application is under maintenance...
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+    require $maintenance;
 }
+
+// Register the Auto Loader...
+require __DIR__.'/../vendor/autoload.php';
+
+// Bootstrap Laravel and handle the request...
+(require_once __DIR__.'/../bootstrap/app.php')
+    ->handleRequest(Request::capture());
